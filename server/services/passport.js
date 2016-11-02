928e3d2dac0901581ba58ffdac70955c5bb2222b
PrakashKoneru <PrakashKoneru@Sivas-MBP.home>
2016-11-01 22:27:13 -0400

    Implemeneted the user.signIn by passing it with req.user

--- server/services/passport.js
+++ server/services/passport.js
@@ -7,18 +7,18 @@ const LocalStrategy = require('passport-local');
 
 
 //create local strategy
-const localOptions = {usernameField: 'email'}
-const localLogin = new LocalStrategy({ localOptions }, function(email, password, done){
+const localOptions = {usernameField: 'email'};
+const localLogin = new LocalStrategy(localOptions , function(email, password, done){
 	// Verify this email and password, call done with the user
 	// if it is the correct email and password
 	//otherwise, call done with false
-	User.findOne({email:email}, function(err){
+	User.findOne({email:email}, function(err, user){
 		if(err) { return done(err); }
 		if (!user) {return done(null, false);}
 
 		//compare passwords- is 'password' equal to user password'
 		user.comparePassword(password, function(err, isMatch){
-			if (err) {return done(null, false);}
+			if (err) {return done(err);}
 			if (!isMatch) {return done(null, false);}
 
 			return done(null, user);
