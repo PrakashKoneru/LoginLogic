4fd72ac4033051f450dc1708ca0e13dc78feb6ab
PrakashKoneru <PrakashKoneru@Sivas-MBP.home>
2016-11-01 21:19:04 -0400

    created exports.signin, implemented the actual method to compare passwords in user.js, created a new signin route with a helper requireSignIn inside routes.jsx and told passport to use local login

--- server/services/passport.js
+++ server/services/passport.js
@@ -9,7 +9,21 @@ const LocalStrategy = require('passport-local');
 //create local strategy
 const localOptions = {usernameField: 'email'}
 const localLogin = new LocalStrategy({ localOptions }, function(email, password, done){
-	
+	// Verify this email and password, call done with the user
+	// if it is the correct email and password
+	//otherwise, call done with false
+	User.findOne({email:email}, function(err){
+		if(err) { return done(err); }
+		if (!user) {return done(null, false);}
+
+		//compare passwords- is 'password' equal to user password'
+		user.comparePassword(password, function(err, isMatch){
+			if (err) {return done(null, false);}
+			if (!isMatch) {return done(null, false);}
+
+			return done(null, user);
+		})
+	})
 });
 
 
@@ -36,3 +50,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
 
 // Tell passport to use this Strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
