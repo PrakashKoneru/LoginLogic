23ac9ff911e4818404f06d5ddf4cf10f7c4770e9
PrakashKoneru <PrakashKoneru@Sivas-MBP.home>
2016-11-01 21:04:51 -0400

    Implemented another passport strategy to handle log in and inside user.js implemented logic to compare the submitted password to the encrypted password

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,14 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
+
+//create local strategy
+const localOptions = {usernameField: 'email'}
+const localLogin = new LocalStrategy({ localOptions }, function(email, password, done){
+	
+});
 
 
 // set up options for Jwt Strategy
